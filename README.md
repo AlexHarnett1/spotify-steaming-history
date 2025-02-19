326bb1516f174648be7c1036c97e89b86892c114
Alex Harnett <harnettalex@gmail.com>
2025-02-18 16:48:10 -0800

    A few queries, instructions to get app running

--- README.md
+++ README.md
@@ -2,3 +2,62 @@
 To start, you must run the following command in your terminal:
 createdb spotifydb
 
+To get server up and running go to root folder and run:
+npx ts-node src/app.ts
+
+To compile public typescripts go to public/src folder and run:
+npx tsc
+
+Sample object of musical track within Spotfy history files:
+{
+  "ts": "2024-05-23T06:49:10Z",
+  "platform": "osx",
+  "ms_played": 220538,
+  "conn_country": "SG",
+  "ip_addr": "38.150.99.14",
+  "master_metadata_track_name": "Go Back (feat. Julia Church)",
+  "master_metadata_album_artist_name": "John Summit",
+  "master_metadata_album_album_name": "Go Back (feat. Julia Church)",
+  "spotify_track_uri": "spotify:track:68R0zVUeMJ2C852Ov6d2Mh",
+  "episode_name": null,
+  "episode_show_name": null,
+  "spotify_episode_uri": null,
+  "audiobook_title": null,
+  "audiobook_uri": null,
+  "audiobook_chapter_uri": null,
+  "audiobook_chapter_title": null,
+  "reason_start": "trackdone",
+  "reason_end": "trackdone",
+  "shuffle": false,
+  "skipped": false,
+  "offline": false,
+  "offline_timestamp": 1716446730,
+  "incognito_mode": false
+}
+
+Sample object of podcast episode play:
+{
+    "ts": "2019-06-17T17:59:03Z",
+    "platform": "iOS 12.1.4 (iPhone9,1)",
+    "ms_played": 1277269,
+    "conn_country": "US",
+    "ip_addr": "66.170.185.132",
+    "master_metadata_track_name": null,
+    "master_metadata_album_artist_name": null,
+    "master_metadata_album_album_name": null,
+    "spotify_track_uri": null,
+    "episode_name": "The Future of Everything Festival: What's Next for Alexa?",
+    "episode_show_name": "WSJ’s The Future of Everything",
+    "spotify_episode_uri": "spotify:episode:2aPctXuvFf4qU2o0FyEPSW",
+    "audiobook_title": null,
+    "audiobook_uri": null,
+    "audiobook_chapter_uri": null,
+    "audiobook_chapter_title": null,
+    "reason_start": "clickrow",
+    "reason_end": "endplay",
+    "shuffle": false,
+    "skipped": false,
+    "offline": false,
+    "offline_timestamp": null,
+    "incognito_mode": false
+  }
--- src/models/listenModels.ts
+++ src/models/listenModels.ts
@@ -38,6 +38,95 @@ async function addListenInstance(instance: ListenInstance) {
       instance.offline, instance.incognito_mode
     ]);
   } catch (error) {
-    console.error(error)
+    console.error('Error adding listen instance:', error);
   }
 }
+
+
+async function getTopSongs() {
+  // This is incorrect. It needs to be grouped by track_name, track_id so that 
+  // songs with the same name aren't grouped together. Ex: Alive
+  // Also need to exclude null for the podcasts
+  const query = `
+    SELECT SUM(ms_played) AS total_ms_played, master_metadata_track_name, spotify_track_uri
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL
+    GROUP BY spotify_track_uri, master_metadata_track_name
+    ORDER BY total_ms_played DESC;`
+}
+
+async function getTopSongsByYear(year: number) {
+  const query = `
+    SELECT SUM(ms_played) AS total_ms_played, master_metadata_track_name, spotify_track_uri
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL AND EXTRACT(YEAR FROM ts) = $1
+    GROUP BY spotify_track_uri, master_metadata_track_name
+    ORDER BY total_ms_played DESC;`
+  try {
+    await db.any(query, year);
+  } catch (error) {
+    console.error('Error getting Top Songs by year', error);
+  }
+}
+
+// This works generally, I feel like there could be some missing instances due to songs
+// being listed as singles and then added to an album later.
+async function getTopAlbums() {
+  const query = `
+    SELECT SUM(ms_played) AS total_ms_played, master_metadata_album_album_name, master_metadata_album_artist_name
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL
+    GROUP BY master_metadata_album_album_name, master_metadata_album_artist_name
+    ORDER BY total_ms_played DESC;
+    `;
+}
+
+
+async function getTopAlbumsByYear(year: number) {
+  const query = `
+    SELECT SUM(ms_played) AS total_ms_played, master_metadata_album_album_name, master_metadata_album_artist_name
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL AND EXTRACT(YEAR FROM ts) = $1
+    GROUP BY master_metadata_album_album_name, master_metadata_album_artist_name
+    ORDER BY total_ms_played DESC;
+    `;
+  try {
+    await db.any(query, year);
+  } catch (error) {
+    console.error('Error getting Top Songs by year', error);
+  }
+  
+}
+
+async function getTopArtists(){
+  const query = `
+    SELECT SUM(ms_played) AS total_ms_played, master_metadata_album_artist_name
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL
+    GROUP BY master_metadata_album_artist_name
+    ORDER BY total_ms_played DESC;
+    `;
+}
+
+async function getTopArtistsByYear() {
+  const query = `
+    SELECT SUM(ms_played) AS total_ms_played, master_metadata_album_artist_name
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL AND EXTRACT(YEAR FROM ts) = $1
+    GROUP BY master_metadata_album_artist_name
+    ORDER BY total_ms_played DESC;
+    `;
+}
+
+async function getMostSkippedSongs() {
+  const query = `
+    SELECT COUNT(*) AS times_skipped, master_metadata_track_name, spotify_track_uri
+    FROM listen_instances
+    WHERE spotify_track_uri IS NOT NULL AND ms_played < 5000 AND EXTRACT(YEAR FROM ts) = 2024
+    GROUP BY spotify_track_uri, master_metadata_track_name
+    ORDER BY times_skipped DESC;
+  `;
+}
+
+
+
